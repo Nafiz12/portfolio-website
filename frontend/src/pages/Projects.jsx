fb545157566773fb77df0488a32df0b587da16ee
Md Nafiz Al Ifat <68993006+Nafiz12@users.noreply.github.com>
2025-09-04 22:19:29 -0400

    Update Projects.jsx

--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -22,7 +22,7 @@ export default function Projects() {
       title: "khelapora",
       description: "The project aims to build a dynamic website with a secure administrative panel. The key features include a user-friendly dashboard, dynamic content management, and role-based access control.",
       link: "https://github.com/Nafiz12/khelapora",
-      label: "GitHub Repo",
+      label: "GitHub Repo", 
     },
     {
       title: "Audit360",
